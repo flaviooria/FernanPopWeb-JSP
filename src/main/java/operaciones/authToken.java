a89f26bf59223b0cb1c7c05a16886665593e3c1a
Flavio Oria <flavio.oriap@gmail.com>
2021-08-07 04:43:54 +0200

    AuthToken ahora validad si el tokenInput pueda ser null o vacio

--- src/main/java/operaciones/authToken.java
+++ src/main/java/operaciones/authToken.java
@@ -24,28 +24,33 @@ public class authToken extends HttpServlet {
         String tokenInput = request.getParameter("token");
         Gestion gestion = null;
 
-        try {
-            gestion = new Gestion();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (gestion != null) {
-            if (tokenInput.trim().equals(tokenUser)) {
-                //autentifico el token
-                Gestion.setAutentificado(user);
-                //Borro el token de la sesión
-                request.getSession().removeAttribute("token");
-                //redirenciono a inicio
-                request.getSession().setAttribute("user", user);
-                response.sendRedirect("./pages/inicio.jsp");
-            } else {
-                request.getSession().setAttribute("failed", "true");
-                response.sendRedirect("./pages/autentificarToken.jsp");
-            }
+        if (tokenInput == null || tokenInput.isEmpty()) {
+            request.getSession().setAttribute("failed", "true");
+            response.sendRedirect("./pages/autentificarToken.jsp");
         } else {
-            request.getSession().setAttribute("error","No se pudo conectar a la base de datos.");
-            response.sendRedirect("./pages/error.jsp");
+            try {
+                gestion = new Gestion();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            if (gestion != null) {
+                if (tokenInput.trim().equals(tokenUser)) {
+                    //autentifico el token
+                    Gestion.setAutentificado(user);
+                    //Borro el token de la sesión
+                    request.getSession().removeAttribute("token");
+                    //redirenciono a inicio
+                    request.getSession().setAttribute("user", user);
+                    response.sendRedirect("./pages/inicio.jsp");
+                } else {
+                    request.getSession().setAttribute("failed", "true");
+                    response.sendRedirect("./pages/autentificarToken.jsp");
+                }
+            } else {
+                request.getSession().setAttribute("error","No se pudo conectar a la base de datos.");
+                response.sendRedirect("./pages/error.jsp");
+            }
         }
     }
 }
